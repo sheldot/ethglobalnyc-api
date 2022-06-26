ef7c113e4fb6d56db52a31f801b8496a28f39a67
Sheldon Trotman <shel.trotman@gmail.com>
2022-06-26 01:15:10 -0400

    slight upate to db table name

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -2,5 +2,5 @@ import env from "./env";
 import { DB_OBJECTS } from "./interfaces";
 
 export const getDbLocationString = (baseDbLocation: DB_OBJECTS) => {
-  return `${env.db.version}-${baseDbLocation}`;
+  return `v${env.db.version}-${baseDbLocation}`;
 };
